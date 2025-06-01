6dc79f64328f7f22e00e70ef84d98556982ae054
Douglas.Mariano <douglas.mariano@estudante.ufjf.br>
2025-06-01 20:06:03 -0300

    teste2Criado

--- WEBII/Ufjf-dcc206-2025-atv1/proximo.test.js
+++ WEBII/Ufjf-dcc206-2025-atv1/proximo.test.js
@@ -1,18 +1,20 @@
-import { test, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { proximo } from './proximo.js';
 
-test('Proximo(0) deve ser 1', () => {
-    expect(proximo(0)).toBe(1);
-});
-test('Proximo(1) deve ser 3', () => {
-    expect(proximo(1)).toBe(3);
-});
-test('Proximo(2) deve ser 5', () => {   
-    expect(proximo(2)).toBe(5);
-});
-test('Proximo(3) deve ser 4', () => {       
-    expect(proximo(3)).toBe(4);
-});
-test('Proximo(4) deve ser 6', () => {
-    expect(proximo(4)).toBe(6);
-});         
+describe('proximo()', async () => {
+    it('deve retornar 1 ao receber 0', () => {
+        expect(proximo(0)).toBe(1);
+    });
+    it('deve retornar 3 ao receber 1', () => {
+        expect(proximo(1)).toBe(3);
+    });
+    it('deve retornar 5 ao receber 2', () => {   
+        expect(proximo(2)).toBe(5);
+    });
+    it('deve retornar 4 ao receber 3', () => {       
+        expect(proximo(3)).toBe(4);
+    });
+    it('deve retonrar 6 ao receber 4', () => {
+        expect(proximo(4)).toBe(6);
+    });         
+});       
